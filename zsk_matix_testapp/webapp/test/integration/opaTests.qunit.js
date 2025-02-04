ffe7646446f567bf41f2896a1e5b2291efa89fec
KumarsantoshPRO <kumar.santosh@protivitglobal.in>
2025-02-04 16:45:46 +0530

    First push

---
+++ zsk_matix_testapp/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["zpro/sk/matix/zskmatixtestapp/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ zsk_matix_testapp/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zproskmatix/zsk_matix_testapp/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
